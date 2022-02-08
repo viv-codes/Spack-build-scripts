e608564c1569ec4e14779735bd4378ce2160a1bf
Vivian Hafener <79525734+viv-codes@users.noreply.github.com>
2022-02-08 13:14:38 -0500

    Completed version functionality in parser

--- parse_requirements_txt.py
+++ parse_requirements_txt.py
@@ -1,9 +1,18 @@
 def main():
+    print('Should version numbers be appended? y/[n]')
+    version = input()
+    if version == "":
+        version = "n"
     with open("requirements.txt") as file:
         Lines = file.readlines()
         for line in Lines:
             output = line.split('==')[0]
-            print("    depends_on('py-" + output +"', type=('build', 'run'))", end="\n")
+            vernum = line.split('==')[1]
+            vernum = vernum.strip()
+            if version == "n":
+                print("    depends_on('py-" + output +"', type=('build', 'run'))", end="\n")
+            elif version == "y":
+                print("    depends_on('py-" + output + "@" +vernum+"', type=('build', 'run'))", end="\n")
 
 if __name__ == "__main__":
     main()
