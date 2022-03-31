176f40e182f91857aa4bf2cc74d057c4d0a84261
viv-codes <vhafener@outlook.com>
2022-03-31 13:00:53 -0400

    Did a bunch more work on binary search

--- dep-binary-search.py
+++ dep-binary-search.py
@@ -1,8 +1,14 @@
 import os
+import sys
 
-def main():
+def query():
+    '''User query for first run only'''
     print("Enter the path for the package.py:")
     path=input()
+    if (verifypath(path) == True):
+        initialprep(path)
+    
+def verifypath(path):
     if path == "":
         print("Must provide an input path! Exiting!")
         exit()
@@ -10,8 +16,34 @@ def main():
         print("Must provide the path for the package.py! Exiting!")
         exit()
     elif os.path.isfile(path):
-        with open(path) as package:
-            lines = package.readlines()
+        return True
+
+def continuitycheck():
+    return os.path.exists(".depbininternal")
+        
+def initialprep(path):
+    if (continuitycheck() == True):
+        print("Tempfile from previous binary search found! Do you wish to overwrite this and start a new search: [y/n]:")
+        choice=input()
+        if (choice.lower() == "y"):
+            pass
+        else:
+            print("Exiting!")
+            exit()
+
+    log=open(".depbsint", "w")
+    with open(path) as package:
+        lines = package.readlines()
+
+def main():
+    if (sys.argv[0] == "" or (sys.argv[0] == "start" and sys.argv[1] == None)): #! I need to confirm that this works as intended
+        print("Starting new binary search!")
+        query()
+    elif (sys.argv[0] == "start" and sys.argv[1] != None):
+        path=sys.argv[1]
+        verifypath(path)
+        initialprep(path)
+    
 
 
 if __name__ == "__main__":
